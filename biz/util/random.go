bab33f9b3e103980b016072a07e069f24506f0f7
guanjian <guanjian@bytedance.com>
2023-04-06 19:42:34 +0800

    完善武将获取，按实际存在武将获取

--- biz/util/random.go
+++ biz/util/random.go
@@ -12,7 +12,7 @@ func Random(min float64, max float64) float64 {
 	return rand.Float64()*(max-min) + cast.ToFloat64(min)
 }
 
-//根据概率设定返回是否成功
+// 根据概率设定返回是否成功
 func GenerateRate(rate float64) bool {
 	rand := Random(0, 1)
 	if rand >= 0 && rand <= rate {
@@ -21,7 +21,7 @@ func GenerateRate(rate float64) bool {
 	return false
 }
 
-//生成totalNum数中命中hitNum数量的索引数组
+// 生成totalNum数中命中hitNum数量的索引数组
 func GenerateHitIdxArr(hitNum, totalNum int) []int64 {
 	idxMap := make(map[int64]bool, 0)
 	for {
@@ -63,13 +63,13 @@ func GenerateHitIdxMap(hitNum, totalNum int) map[int64]bool {
 	return idxMap
 }
 
-//生成total数中的索引返回
+// 生成total数中的索引返回
 func GenerateHitOneIdx(total int) int64 {
 	rand := Random(0, float64(total))
 	return int64(rand)
 }
 
-//生成total数中的索引返回
+// 生成total数中的索引返回
 func GenerateHitTwoOrThreeIdxArr() []int64 {
 	if GenerateRate(0.5) {
 		return GenerateHitIdxArr(2, 3)
@@ -77,14 +77,25 @@ func GenerateHitTwoOrThreeIdxArr() []int64 {
 	return GenerateHitIdxArr(3, 3)
 }
 
-//生成total数中的索引返回
-func GenerateHitTwoOrThreeIdxMap() map[int64]bool {
+// 生成total数中的索引返回
+func GenerateHitTwoOrThreeIdxMap(totalNum int) map[int64]bool {
 	arr := make([]int64, 0)
 	m := make(map[int64]bool, 0)
-	if GenerateRate(0.5) {
-		arr = GenerateHitIdxArr(2, 3)
-	} else {
-		arr = GenerateHitIdxArr(3, 3)
+
+	if totalNum > 3 && totalNum < 0 {
+		panic("totalNum is err")
+	}
+
+	if totalNum == 1 { //1人
+		arr = GenerateHitIdxArr(1, 1)
+	} else if totalNum == 2 { //2人
+		arr = GenerateHitIdxArr(2, 2)
+	} else { //3人
+		if GenerateRate(0.5) {
+			arr = GenerateHitIdxArr(2, 3)
+		} else {
+			arr = GenerateHitIdxArr(3, 3)
+		}
 	}
 	for _, idx := range arr {
 		m[idx] = true

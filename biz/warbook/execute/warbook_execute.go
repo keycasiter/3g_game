af0db1acd65e3b886883a92011d80f1a4c8a2822
关键 <guanjian@bytedance.com>
2024-11-10 18:14:42 +0800

    add log

--- biz/warbook/execute/warbook_execute.go
+++ biz/warbook/execute/warbook_execute.go
@@ -3,6 +3,7 @@ package execute
 import (
 	"context"
 
+	"github.com/cloudwego/hertz/pkg/common/hlog"
 	"github.com/keycasiter/3g_game/biz/consts"
 	"github.com/keycasiter/3g_game/biz/model/vo"
 )
@@ -18,6 +19,7 @@ func NewWarBookExecutor(ctx context.Context, general *vo.BattleGeneral) *WarBook
 
 func (w *WarBookExecutor) Execute() {
 	for _, warbook := range w.General.WarBooks {
+		hlog.CtxInfof(w.Ctx, "[%v]兵书[%v]效果处理...", w.General.BaseInfo.Name, warbook.Name)
 		if handler, ok := WarbookHandlerMap[consts.WarBookDetailType(warbook.Id)]; ok {
 			handler.Handle(w.Ctx, w.General)
 		}

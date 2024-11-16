e70c0b576888619ac629a95765ba82e503d94014
关键 <guanjian@bytedance.com>
2024-11-16 21:56:20 +0800

    update

--- biz/logic/battle/battle_list_logic.go
+++ biz/logic/battle/battle_list_logic.go
@@ -7,7 +7,6 @@ import (
 	"github.com/keycasiter/3g_game/biz/dal/mysql"
 	"github.com/keycasiter/3g_game/biz/model/api"
 	"github.com/keycasiter/3g_game/biz/model/enum"
-	"github.com/keycasiter/3g_game/biz/model/vo"
 	"github.com/keycasiter/3g_game/biz/util"
 )
 
@@ -40,19 +39,19 @@ func (g *BattleListLogic) Handle() (api.BattleListResponse, error) {
 	resList := make([]*api.BattleRecordInfo, 0)
 	for _, record := range list {
 		//反序列化
-		battleRecord := &BattleLogicV2ContextResponse{}
+		battleRecord := &api.BattleDoResponse{}
 		util.ParseJsonObj(g.Ctx, battleRecord, record.BattleRecord)
 
 		resList = append(resList, &api.BattleRecordInfo{
 			BattleResult: enum.BattleResult(battleRecord.BattleResultStatistics.FightingTeam.BattleResult),
 			FightingTeam: &api.TeamBattleStatistics{
 				BattleTeam: &api.BattleTeamStatistics{
-					BattleGenerals: buildBattleGenerals(battleRecord.BattleResultStatistics.FightingTeam.BattleTeam.BattleGenerals),
+					BattleGenerals: battleRecord.BattleResultStatistics.FightingTeam.BattleTeam.BattleGenerals,
 				},
 			},
 			EnemyTeam: &api.TeamBattleStatistics{
 				BattleTeam: &api.BattleTeamStatistics{
-					BattleGenerals: buildBattleGenerals(battleRecord.BattleResultStatistics.EnemyTeam.BattleTeam.BattleGenerals),
+					BattleGenerals: battleRecord.BattleResultStatistics.EnemyTeam.BattleTeam.BattleGenerals,
 				},
 			},
 		})
@@ -62,17 +61,4 @@ func (g *BattleListLogic) Handle() (api.BattleListResponse, error) {
 		BattleRecordList: resList,
 	}
 	return g.Resp, nil
-}
-
-func buildBattleGenerals(generals []*vo.BattleGeneral) []*api.BattleGeneralStatistics {
-	list := make([]*api.BattleGeneralStatistics, 0)
-	for _, general := range generals {
-		list = append(list, &api.BattleGeneralStatistics{
-			BaseInfo: &api.MetadataGeneral{
-				Name:      general.BaseInfo.Name,
-				AvatarUrl: general.BaseInfo.AvatarUri,
-			},
-		})
-	}
-	return list
 }
